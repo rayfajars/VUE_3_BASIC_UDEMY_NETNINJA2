3950c6bd44321a74fc12671bbf5186cd1597acab
Ray Fajar S <rayfajars.com>
2022-01-25 07:20:41 +0700

    add value string from app vue to components with props

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -1,45 +1,42 @@
 <template>
   <div class="backdrop">
-      <div class="modal">
-          <h1>Modal Title</h1>
-          <p>modal content</p>
-      </div>
+    <div class="modal">
+      <h1>{{ header }}</h1>
+      <p>{{ text }}</p>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-
-}
+  props: ["header", "text"],
+};
 </script>
-
-
 
 <style>
 .modal {
-    width: 400px;
-    padding:20px;
-    margin:100px auto;
-    background: white;
-    border-radius: 10px;
+  width: 400px;
+  padding: 20px;
+  margin: 100px auto;
+  background: white;
+  border-radius: 10px;
 }
 
-.backdrop{
-    top: 0;
-    position: fixed;
-    background: rgba(0, 0, 0, 0.5);
-    width: 100%;
-    height: 100%;
+.backdrop {
+  top: 0;
+  position: fixed;
+  background: rgba(0, 0, 0, 0.5);
+  width: 100%;
+  height: 100%;
 }
 
-h1{
-    color:#03cfb4;
-    border:none;
-    padding: 0;
+h1 {
+  color: #03cfb4;
+  border: none;
+  padding: 0;
 }
 
-
-.modal p{
-    font-style: normal;
+.modal p {
+  font-style: normal;
 }
 </style>
