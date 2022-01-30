f2ef6ced1ccf167d1a3b2197d14b9689f146e5ec
Ray Fajar S <rayfajars.com>
2022-01-30 18:54:35 +0700

    mengirimkan function dari app vue ke components , atau mengirimkan function dari template ke components

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,11 @@
 <template>
   <h1>{{ title }}</h1>
-  <Modal :header='header' :text='text' theme="sale" />
+  <p>Welcome...</p>
+  <div v-if="showModal">
+    <Modal :header='header' :text='text' theme="sale" @close="toggleModal" />
+  </div>
+  <button @click="toggleModal">Open Modal</button>
+
 </template>
 
 <script>
@@ -16,15 +21,14 @@ export default {
     return {
       title: "My First Vue App",
       header:'Sign up for the Giveaway!',
-      text:'Grab your giveaway'
+      text:'Grab your giveaway',
+      showModal:false
     };
   },
   methods: {
-    handleClick() {
-      console.log(this.$refs.name);
-      this.$refs.name.classList.add("active");
-      this.$refs.name.focus();
-    },
+    toggleModal(){
+      this.showModal = !this.showModal;
+    }
   },
 };
 </script>
