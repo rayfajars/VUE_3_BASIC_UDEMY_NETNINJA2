5331e877311a3941fd246b41e80a5045f85d15ae
Ray Fajar S <rayfajars.com>
2022-01-30 19:28:29 +0700

    challenge membuat 2 modal dengan fucntion berbeda

--- src/App.vue
+++ src/App.vue
@@ -2,17 +2,24 @@
   <h1>{{ title }}</h1>
   <p>Welcome...</p>
   <div v-if="showModal">
-    <Modal  theme="" @close="toggleModal">
+    <Modal theme="" @close="toggleModal">
       <template v-slot:links>
         <a href="#">sign up now</a>
         <a href="#">more info</a>
       </template>
-       <h1>Ray Fajars</h1>
-       <p>Lorem ipsum dolor sit amet.</p>
-       
+      <h1>Ray Fajars</h1>
+      <p>Lorem ipsum dolor sit amet.</p>
+    </Modal>
+  </div>
+
+   <div v-if="showModalTwo">
+    <Modal  @close="toggleModalTwo">
+      <h1>Sign up to the newsletter</h1>
+      <p>for updates and promo codes!</p>
     </Modal>
   </div>
   <button @click.alt="toggleModal">Open Modal (alt)</button>
+  <button @click="toggleModalTwo">Open Modal</button>
 </template>
 
 <script>
@@ -27,13 +34,15 @@ export default {
   data() {
     return {
       title: "My First Vue App",
-  
       showModal: false,
+      showModalTwo:false
     };
   },
   methods: {
     toggleModal() {
       this.showModal = !this.showModal;
+    },toggleModalTwo() {
+      this.showModalTwo = !this.showModalTwo;
     },
   },
 };
